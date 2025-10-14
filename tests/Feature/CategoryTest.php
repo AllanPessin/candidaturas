0149fbed93fd2b3fb69b6aacbd112adb53985ffe
AllanPessin <allan.pessin@outlook.com>
2025-10-14 17:12:36 -0300

    test: testes de categorias finalizados

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -29,9 +29,143 @@ class CategoryTest extends TestCase
                 'message' => 'Category created successfully',
             ]);
 
-        $this->assertDataBaseHas('categories', [
+        $this->assertDatabaseHas('categories', [
             'name' => $category->name,
             'slug' => $category->slug,
         ]);
+    }
+
+    public function test_list_categories()
+    {
+        $this->authenticated();
+
+        Category::factory()->count(3)->create();
+
+        $this->getJson('/api/categories')
+            ->assertOk()
+            ->assertJsonCount(3, 'data')
+            ->assertJson([
+                'data' => [
+                    ['id' => 1],
+                    ['id' => 2],
+                    ['id' => 3],
+                ],
+            ]);
+    }
+
+    public function test_can_update_category()
+    {
+        $this->authenticated();
+
+        $category = Category::factory()->create([
+            'name' => 'New category',
+        ]);
+
+        $this->putJson("/api/categories/{$category->slug}", [
+            'name' => 'Updated category',
+            'slug' => 'updated-category',
+        ])
+            ->assertOk()
+            ->assertJson([
+                'data' => [
+                    'id' => $category->id,
+                    'name' => 'Updated category',
+                    'slug' => 'updated-category',
+                ],
+            ]);
+
+        $this->assertDatabaseHas('categories', [
+            'id' => $category->id,
+            'name' => 'Updated category',
+            'slug' => 'updated-category',
+        ]);
+    }
+
+    public function test_category_can_be_searchable()
+    {
+        $this->authenticated();
+
+        Category::factory()->create(['name' => 'Category one']);
+        Category::factory()->create(['name' => 'Category two']);
+
+        $this->getJson('/api/categories?search=category')
+            ->assertOk()
+            ->assertJsonCount(2, 'data')
+            ->assertJson([
+                'data' => [
+                    ['name' => 'Category one'],
+                ],
+            ]);
+    }
+
+    public function test_list_one_category()
+    {
+        $this->authenticated();
+
+        $category = Category::factory()->create();
+
+        $this->getJson("/api/categories/{$category->slug}")
+            ->assertOK()
+            ->assertJson([
+                'data' => [
+                    'id' => $category->id,
+                    'name' => $category->name,
+                    'slug' => $category->slug,
+                ],
+            ]);
+    }
+
+    public function test_delete_category()
+    {
+        $this->authenticated();
+
+        $category = Category::factory()->create();
+
+        $this->deleteJson("/api/categories/{$category->slug}")
+            ->assertOk()
+            ->assertJson([
+                'message' => 'Category deleted successfully',
+            ]);
+
+        $this->assertDatabaseMissing('categories', [
+            'slug' => $category->slug
+        ]);
+    }
+
+    public function test_canoot_create_duplicated_category_slug()
+    {
+        $this->authenticated();
+
+        $category = Category::factory()->create();
+
+        $this->postJson('/api/categories', [
+            'name' => 'Another category',
+            'slug' => $category->slug,
+        ])
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors(['slug']);
+    }
+
+    public function test_cannot_update_category_to_existing_slug()
+    {
+        $this->authenticated();
+
+        $category1 = Category::factory()->create();
+        $category2 = Category::factory()->create();
+
+        $this->putJson("/api/categories/{$category2->slug}", [
+            'name' => 'Updated name',
+            'slug' => $category1->slug,
+        ])
+            ->assertUnprocessable()
+            ->assertJsonValidationErrors(['slug']);
+    }
+
+    public function test_cannot_delete_non_existing_status()
+    {
+        $this->authenticated();
+
+        $this->deleteJson('/api/categories/non-existing-slug')
+            ->assertNotFound();
     }
 }
--- tests/Unit/CategoryUnitTest.php
+++ tests/Unit/CategoryUnitTest.php
@@ -2,15 +2,38 @@
 
 namespace Tests\Unit;
 
-use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\Store\StoreCategoryRequest;
+use App\Http\Requests\Update\UpdateCategoryRequest;
+use App\Models\Category;
+use Illuminate\Support\Str;
 use PHPUnit\Framework\TestCase;
 
 class CategoryUnitTest extends TestCase
 {
     public function test_authorize_store_category_request()
     {
-        $storeCategoryRequest = new StoreCategoryRequest();
+        $storeCategoryRequest = new StoreCategoryRequest;
 
         $this->assertTrue($storeCategoryRequest->authorize());
+    }
+
+    public function test_returns_slug_as_route_key_name()
+    {
+        $category = new Category;
+
+        $this->assertEquals('slug', $category->getRouteKeyName());
+    }
+
+    public function test_converts_slug_to_a_url_friendly_format_before_save()
+    {
+        $updateCategoryRequest = new UpdateCategoryRequest;
+
+        $updateCategoryRequest->merge([
+            'slug' => 'This is a test slug',
+        ]);
+
+        $updateCategoryRequest->prepareForValidation();
+
+        $this->assertEquals(Str::slug('This is a test slug'), $updateCategoryRequest->slug);
     }
 }
